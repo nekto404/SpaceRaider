a7adb324323a76ac2fed5c291226f49c4b0cbb87
Roman <gsmartrsh@gmail.com>
2018-10-26 17:56:04 +0300

    Weapon in work

---
+++ Assets/Scripts/Missle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missle : MonoBehaviour
+{
+    [SerializeField] private Vector2 direction;
+    [SerializeField] private float speed;
+    [SerializeField] private float damage;
+    [SerializeField] private bool isPlayerMilles;
+
+    public void Start()
+    {
+        direction = direction.normalized;
+    }
+
+    public void InitValues(Vector2 directon, float speed, float damage, bool isPlayerMilles)
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(direction.x, direction.y) * speed * Time.deltaTime;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.tag);
+        if (collision.gameObject.tag == "Border")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
