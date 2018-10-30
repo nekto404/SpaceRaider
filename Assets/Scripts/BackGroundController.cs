03c3199fa35e59d5b7a1c11a15b4c10317c7e5c5
Roman <gsmartrsh@gmail.com>
2018-10-30 18:00:09 +0200

    Add backgrounds

---
+++ Assets/Scripts/BackGroundController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundController : MonoBehaviour
+{
+    public List<GameObject> Sprites;
+    public float MinX;
+    public float MinY;
+    public float StepX;
+    public float BackGroundSpeed;
+
+    void Awake()
+    {
+        var position = new Vector3(MinX , 0);
+        foreach (var sprite in Sprites)
+        {
+            sprite.transform.position = position;
+            position += new Vector3(-StepX , MinY);
+        }
+    }
+
+	void Update ()
+	{
+	    foreach (var sprite in Sprites)
+	    {
+	        NewSpritePosition(sprite);
+	    }
+    }
+
+    public void NewSpritePosition(GameObject sprite)
+    {
+        sprite.transform.position -= new Vector3(0, BackGorundSpeedCalculation());
+        if (sprite.transform.position.y > -MinY) return;
+        sprite.transform.position += new Vector3(-StepX, MinY * 2);
+        if (sprite.transform.position.x > -MinX) return;
+        sprite.transform.position += new Vector3(MinX * 2, 0);
+    }
+
+    public float BackGorundSpeedCalculation()
+    {
+        return BackGroundSpeed * GameData.Incstance.SpeedKof * Time.deltaTime;
+    }
+}
