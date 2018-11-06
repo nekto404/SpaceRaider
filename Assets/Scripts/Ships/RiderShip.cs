a059d6496e4e4a9edabbb492714a685290e6a8b4
Roman <gsmartrsh@gmail.com>
2018-11-06 22:48:33 +0200

    Start  work on RiderShip

---
+++ Assets/Scripts/Ships/RiderShip.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEditor;
+using UnityEngine;
+
+public class RiderShip : EnemyShip
+{
+    [SerializeField]
+    private List<GameObject> shipPath;
+    [SerializeField]
+    private float fireTime;
+    [SerializeField]
+    private float rechargeTime;
+
+    private float period;
+    private float curentT;
+
+    void Start()
+    {
+        curentT = 0;
+        period = fireTime + rechargeTime;
+    }
+
+    public override void ShipLogic(float deltaTime)
+    {
+        curentT += deltaTime;
+        if (curentT > period)
+        {
+            curentT -= period;
+        }
+
+        if (curentT > rechargeTime)
+        {
+            Fire();
+        }
+    }
+}
